ff10c961a2630c7e8f73f496fa653e7c3e99868e
ccmmail <ccmmail@gmail.com>
2026-04-10 19:00:01 -0700

    Auto-update: 2026-04-10 19:00:01

--- scripts/auto_git_push.sh
+++ scripts/auto_git_push.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-set -e
+set -e 
+export PATH="/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+# log invocation
+LOG="/Users/ccm/Documents/projects/sensemaking-agent/scripts/auto_git_push.log"
+echo "=== cron invoked at $(date) ===" >> "$LOG"
 
 # replace with your directory location
-REPO="$HOME/documents/projects/sensemaking-agent"
+REPO="$HOME/Documents/projects/sensemaking-agent"
 BRANCH="main"
 
 # make sure we are in the right repo
